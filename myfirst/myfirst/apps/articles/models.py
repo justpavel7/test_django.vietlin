82f05ff9fa0db7479f8273554e3df166b13a0177
Pavel Vietlin <justpave@gmail.com>
2022-06-19 13:57:43 +0300

    three variables responsible for the title text and time of publication

--- myfirst/myfirst/apps/articles/models.py
+++ myfirst/myfirst/apps/articles/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Article(models.Model):
-    pass
+    article_title = models.CharField('название статьи', max_length=200)
+    article_text = models.TextField('текст статьи')
+    pub_date = models.DateTimeField('дата публикации')
 
 class Comment(models.Model):
     pass
