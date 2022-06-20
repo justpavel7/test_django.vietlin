949397e9fb04a77fcc9899a3f35ba6a84987c44d
Pavel Vietlin <justpave@gmail.com>
2022-06-20 22:03:46 +0300

    python manage.py collectstatic

--- myfirst/myfirst/settings.py
+++ myfirst/myfirst/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
@@ -126,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
