e79f60bdbdcc6ac2f44f4e93e7ac3848e6cdb1b8
Pavel Vietlin <justpave@gmail.com>
2022-06-22 12:42:51 +0300

    output of articles on id

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 
 from .models import Article, Comment
@@ -5,3 +6,10 @@ from .models import Article, Comment
 def index(request):
     latest_articles_list = Article.objects.order_by('-pub_date')[:5]
     return render(request, 'articles/list.html', {'latest_articles_list': latest_articles_list})
+def detail(request, article_id):
+    try:
+        a = Article.objects.get( id = article_id)
+    except:
+        raise Http404("Статья не найдена")
+
+    return render(request, 'articles/detail.html', {'article': a})
