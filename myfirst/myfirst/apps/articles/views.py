586d63b4b81a084cc4871022ec9243e0d3e2ea35
Pavel Vietlin <justpave@gmail.com>
2022-06-22 12:11:27 +0300

    corrected latest

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from .models import Article, Comment
 
 def index(request):
-    lates_articles_list = Article.objects.order_by('-pub_date')[:5]
-    return render(request, 'articles/list.html', {'lates_articles_list': lates_articles_list})
+    latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+    return render(request, 'articles/list.html', {'latest_articles_list': latest_articles_list})
