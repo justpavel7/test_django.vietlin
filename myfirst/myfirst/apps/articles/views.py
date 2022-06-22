9ea80c8ff9af3d863a071ad9629d010a92756163
Pavel Vietlin <justpave@gmail.com>
2022-06-22 11:49:19 +0300

    del test

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -1,7 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello world")
-
-def test(request):
-    return HttpResponse('Test')
+    return render(request, 'articles/list.html')
